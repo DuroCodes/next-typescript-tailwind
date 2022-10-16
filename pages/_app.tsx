8ac4f2bb39c6881239c37e1722fa42b397d5e8b8
DuroCodes <davidwright13503@gmail.com>
2022-10-16 15:43:59 -0400

    feat: setup tailwind & next-themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles.css';
+import '../custom.css';
+
+import { ThemeProvider } from 'next-themes';
+import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" forcedTheme="dark">
+      <>
+        <svg height="0px" width="0px">
+          <defs>
+            <linearGradient
+              id="pink-gradient"
+              x1="0%"
+              y1="0%"
+              x2="100%"
+              y2="100%"
+            >
+              <stop offset="0%" stopColor="rgba(156, 81, 161, 1)" />
+              <stop offset="70%" stopColor="rgba(255, 30, 86, 1)" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
